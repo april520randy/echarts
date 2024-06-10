03ddeb3dd62ca1139e322dc85ea83b2fa6d18ff3
randy <randyokvip@gmail.com>
2024-06-10 16:56:03 +0800

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteConfigCommon from './vite.config.common';
 // import visualizer from 'rollup-plugin-visualizer';
 
 export default defineConfig({
+  base: './',
   server: {
     hmr: false,
   },
